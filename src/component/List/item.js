f03e7eb9326e679bb81391332f4a212247ff2407
ismailaissami <aissami.ismail@gmail.com>
2022-03-03 09:59:35 +0100

    first commit

---
+++ src/component/List/item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, useColorScheme, View} from 'react-native';
+
+const Element = () => {
+  return(
+      <View>
+          <Text>item</Text>
+      </View>
+  );
+};
+
+export default Element;
